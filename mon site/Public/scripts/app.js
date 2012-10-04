9cf39bede73e6af2190164b3c9fb60ee47d87271
Noël Flanter <flower-is-truth@hotmail.fr>
2012-10-04 12:56:17 +0200

    J'ai ajouté 2 lignes concernant des javascripts

---
+++ mon site/Public/scripts/app.js
@@ -0,0 +1,9 @@
+function check_name(name){
+	if( name == "gabriel"){
+		console.log("Win!");
+	}else if(name == "Clement"){
+		console.log("We found Clement!");
+	}else{
+		console.log("Truth!"+ name + "is different from gabriel and Clement!");
+	}
+}
